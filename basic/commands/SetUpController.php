bf68586e1b70bf886730a1a5069c7726dc3e7295
rolenweb <rolenweb@yandex.ru>
2016-12-03 12:45:40 +0300

    setup

--- basic/commands/SetUpController.php
+++ basic/commands/SetUpController.php
@@ -18,7 +18,7 @@ use yii\console\Controller;
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
-class SetUpController extends Controller
+class SetUpController extends BaseCommand
 {
     /**
      * This command echoes what you have entered as the message.
@@ -26,6 +26,14 @@ class SetUpController extends Controller
      */
     public function actionIndex()
     {
-        var_dump(Yii::$app->request->baseUrl);
+    	$this->success('Creation dir: '.Yii::$app->basePath. DIRECTORY_SEPARATOR .'cookiefile');
+    	mkdir(Yii::$app->basePath. DIRECTORY_SEPARATOR .'cookiefile',0777);
+    	$this->success('Creation dir: '.Yii::$app->basePath. DIRECTORY_SEPARATOR .'recognize');
+    	mkdir(Yii::$app->basePath. DIRECTORY_SEPARATOR .'recognize',0777);
+    	$this->success('Creation dir: '.Yii::$app->basePath. DIRECTORY_SEPARATOR .'commands'. DIRECTORY_SEPARATOR .'files');
+    	mkdir(Yii::$app->basePath. DIRECTORY_SEPARATOR .'commands'. DIRECTORY_SEPARATOR .'files',0777);
+    	$this->success('Creation dir: '.Yii::$app->basePath. DIRECTORY_SEPARATOR .'commands'. DIRECTORY_SEPARATOR .'files'. DIRECTORY_SEPARATOR .'mail');
+    	mkdir(Yii::$app->basePath. DIRECTORY_SEPARATOR .'commands'. DIRECTORY_SEPARATOR .'files'. DIRECTORY_SEPARATOR .'mail',0777);
+        
     }
 }
